17ed734b48a0624d8e1ead3e251d210ae5c87247
VictoriaGorobets <vickyhome1996@gmail.com>
2024-01-25 12:57:39 +0200

    fix: fix bag

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,13 +2,17 @@ const fs = require('fs');
 const path = require('path');
 
 (function copyDir() {
-  fs.readdir(path.join('./files'), { withFileTypes: true }, (error, files) => {
-    files
-      .filter((file) => file.isFile() && !file.name.startsWith('.'))
-      .forEach((file) => {
-        const filePath = file.path + '/' + file.name;
-        const distFilePath = './files-copy/' + file.name;
-        fs.copyFile(filePath, distFilePath, (err) => console.error(err));
-      });
-  });
+  fs.readdir(
+    path.join(__dirname, 'files'),
+    { withFileTypes: true },
+    (error, files) => {
+      files
+        .filter((file) => file.isFile() && !file.name.startsWith('.'))
+        .forEach((file) => {
+          const filePath = file.path + '/' + file.name;
+          const distFilePath = './files-copy/' + file.name;
+          fs.copyFile(filePath, distFilePath, (err) => console.error(err));
+        });
+    },
+  );
 })();
