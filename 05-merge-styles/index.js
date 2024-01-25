ef8778e21723d9e9c95e158d8deb0099525b5a06
VictoriaGorobets <vickyhome1996@gmail.com>
2024-01-25 20:59:52 +0200

    fix bag

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,20 +1,23 @@
 const fs = require('fs');
 const path = require('node:path');
 
-const bundleFilePath = path.join(__dirname, 'project-dist/bundle.css');
+const distDir = path.join(__dirname, 'project-dist');
+!fs.existsSync(distDir) ? fs.mkdirSync(distDir) : false;
+const bundleFilePath = path.join(distDir, 'bundle.css');
 const bundleStream = fs.createWriteStream(bundleFilePath);
 fs.readdir(
   path.join(__dirname, 'styles'),
   { withFileTypes: true },
   (error, files) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
     const fileDirs = files
       .filter((file) => file.isFile() && file.name.endsWith('.css'))
-      .map((file) => path.join(file.path + '/' + file.name));
-
-    bundleStream.write('');
+      .map((file) => path.join(__dirname, 'styles', file.name));
     fileDirs.forEach((fileDir) => {
-      const textFileDir = path.join(__dirname, fileDir);
-      const stream = fs.createReadStream(textFileDir);
+      const stream = fs.createReadStream(fileDir);
       stream.on('readable', function () {
         const data = stream.read()?.toString();
         if (data) {
