5f05cbe7273db64ea7a04e0ea89587f093b228a3
VictoriaGorobets <vickyhome1996@gmail.com>
2024-01-25 13:12:31 +0200

    fix: fix bags

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,22 +1,26 @@
 const fs = require('fs');
 const path = require('node:path');
 
-const bundleFilePath = path.join(__dirname, '/project-dist/bundle.css');
+const bundleFilePath = path.join(__dirname, 'project-dist/bundle.css');
 const bundleStream = fs.createWriteStream(bundleFilePath);
-fs.readdir('./styles', { withFileTypes: true }, (error, files) => {
-  const fileDirs = files
-    .filter((file) => file.isFile() && file.name.endsWith('.css'))
-    .map((file) => file.path + '/' + file.name);
+fs.readdir(
+  path.join(__dirname, 'styles'),
+  { withFileTypes: true },
+  (error, files) => {
+    const fileDirs = files
+      .filter((file) => file.isFile() && file.name.endsWith('.css'))
+      .map((file) => path.join(file.path + '/' + file.name));
 
-  bundleStream.write('');
-  fileDirs.forEach((fileDir) => {
-    const textFileDir = path.join(__dirname, fileDir);
-    const stream = fs.createReadStream(textFileDir);
-    stream.on('readable', function () {
-      const data = stream.read()?.toString();
-      if (data) {
-        bundleStream.write(data);
-      }
+    bundleStream.write('');
+    fileDirs.forEach((fileDir) => {
+      const textFileDir = path.join(__dirname, fileDir);
+      const stream = fs.createReadStream(textFileDir);
+      stream.on('readable', function () {
+        const data = stream.read()?.toString();
+        if (data) {
+          bundleStream.write(data);
+        }
+      });
     });
-  });
-});
+  },
+);
